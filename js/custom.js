e2cadc9210e1763dc6dcac5e33e3079b7a3bc9e4
Goran Horvat <goran@lab.co.uk>
2016-06-08 10:25:13 +0200

    New Header and footer

--- js/custom.js
+++ js/custom.js
@@ -1,30 +1,23 @@
-jQuery(function () {
+jQuery(document).ready(function () {
+    stickyHeader();
+
+});
+function stickyHeader() {
     jQuery(window).scroll(function () {
-        var hh = jQuery("#navbar .container-fluid").outerHeight();
-        if (jQuery(this).scrollTop() > 50) {
-            jQuery('#navbar').css({
-                'position' :'fixed',
-                'width': '100%',
-                'top' : 0
-            });
-
-            jQuery("#navbar").addClass("sticky");
-            jQuery("#header_placeholder").removeClass("hidden").css("height", hh);
-            jQuery(".logo .content .img-purple").addClass("hidden");
-            jQuery(".logo .content .img-white").removeClass("hidden");
-
-        } else if (jQuery(this).scrollTop() <= 50) {
-            jQuery('#navbar').css('position', 'relative');
-            // jQuery('#navbar').slideUp();
-            jQuery("#navbar").removeClass("sticky");
-            jQuery("#header_placeholder").addClass("hidden");
-            jQuery(".logo .content .img-white").addClass("hidden");
-            jQuery(".logo .content .img-purple").removeClass("hidden");
-
-
+        if (jQuery(document).scrollTop() > 0) {
+            jQuery('#navbar').addClass('sticky');
+        } else {
+            jQuery('#navbar').removeClass('sticky');
         }
     });
-
-
+}
+jQuery(function () {
+    var reposition = function (el, after) {
+        jQuery(el).detach().insertAfter(jQuery(after));
+    };
+    reposition(".sm-number", "#block-coachhire-help-logo");
+    jQuery(".navbar-toggle").click(function () {
+        jQuery(this).toggleClass("collapsed");
+    })
 
 });
